3a2cf6f594e3989a7c74915b3b5cfe45267bf797
Ayush Jain <ayushjain2205@gmail.com>
2021-06-27 04:56:52 +0530

    first push (explore page)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./bootstrap.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Explore from "./Components/Explore";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/explore' render={(props) => <Explore />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
